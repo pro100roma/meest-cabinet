0d70d8316f3d3dad524cb0a8ac9c04e890b67e61
unknown <annahrishko@ukr.net>
2022-09-11 23:29:41 +0300

    validate form

--- front/js/script.js
+++ front/js/script.js
@@ -26,6 +26,88 @@ $(function () {
 
 
 
+  $(function ()
+{
+    $.validator.addMethod("loginRegex", function(value, element) {
+        return this.optional(element) || /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/i.test(value);
+    }, "Username must contain only letters, numbers, or dashes.");
+
+    $.validator.addMethod("passwordRegex", function(value, element) {
+        return this.optional(element) || /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/i.test(value);
+    }, "Username must contain only letters, numbers, or dashes.");
+
+    $(".login-form-valid").validate({
+        rules: {
+            "email": {
+                required: true,
+                loginRegex: true,
+            },
+            "password": {
+                required: true,
+                passwordRegex: true
+
+            }
+        },
+        messages: {
+            "email": {
+                required: "You must enter a login name",
+                loginRegex: "Login format not valid"
+            },
+            "password": {
+                required: "",
+            }
+        }
+    });
+});
+
+
+$(function ()
+{
+    $.validator.addMethod("loginRegex", function(value, element) {
+        return this.optional(element) || /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/i.test(value);
+    }, "Username must contain only letters, numbers, or dashes.");
+
+    $.validator.addMethod("passwordRegex", function(value, element) {
+        return this.optional(element) || /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/i.test(value);
+    }, "Username must contain only letters, numbers, or dashes.");
+
+    $(".form-create-account").validate({
+        rules: {
+            "email": {
+                required: true,
+                loginRegex: true,
+            },
+            "password": {
+                required: true,
+                passwordRegex: true
+
+            }
+        },
+        messages: {
+            "email": {
+                required: "You must enter a login name",
+                loginRegex: "Login format not valid"
+            },
+            "password": {
+                required: "",
+            }
+        }
+    });
+});
+
+
+$(".phone").intlTelInput({
+  allowDropdown: true,
+    // options here
+});
+
+
+
+
+
+
+
+
 
 
 
