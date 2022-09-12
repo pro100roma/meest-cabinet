382d3d9fb19ee00dc964937dc1cbd2779bb7d803
unknown <annahrishko@ukr.net>
2022-09-12 16:55:04 +0300

    createAccount validate form second

--- dev/js/script.js
+++ dev/js/script.js
@@ -8,10 +8,10 @@ $(function() {
     $(".section-sub-menu").removeClass('active')
     $(this).next(".section-sub-menu").addClass('active')
      })
-  })
+});
 
 
-$(function () {
+$(function() {
   $('.popup-modal').magnificPopup({
     type: 'inline',
     preloader: false,
@@ -24,11 +24,8 @@ $(function () {
 });
 
 
-
-
-  $(function ()
-{
-    $.validator.addMethod("loginRegex", function(value, element) {
+$(function() {
+    $.validator.addMethod("emailRegex", function(value, element) {
         return this.optional(element) || /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/i.test(value);
     }, "Username must contain only letters, numbers, or dashes.");
 
@@ -40,7 +37,7 @@ $(function () {
         rules: {
             "email": {
                 required: true,
-                loginRegex: true,
+                emailRegex: true,
             },
             "password": {
                 required: true,
@@ -50,60 +47,53 @@ $(function () {
         },
         messages: {
             "email": {
-                required: "You must enter a login name",
-                loginRegex: "Login format not valid"
+                required: "You must enter an email",
+                emailRegex: "Email format not valid"
             },
             "password": {
-                required: "",
+                required: "Password is reguired",
+                passwordRegex: "Username must contain only letters, numbers, or dashes.",
             }
         }
     });
 });
 
 
-$(function ()
-{
-    $.validator.addMethod("loginRegex", function(value, element) {
-        return this.optional(element) || /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/i.test(value);
-    }, "Username must contain only letters, numbers, or dashes.");
-
-    $.validator.addMethod("passwordRegex", function(value, element) {
-        return this.optional(element) || /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/i.test(value);
-    }, "Username must contain only letters, numbers, or dashes.");
-
+$(function() {
     $(".form-create-account").validate({
         rules: {
             "email": {
                 required: true,
-                loginRegex: true,
+                emailRegex: true,
             },
             "password": {
                 required: true,
                 passwordRegex: true
-            }
+            },
             "password_confirm": {
+              required: true,
               equalTo : '[name="password"]'
-            }
+            },
             "phone": {
               required: true,
-            }
+            },
             "last_name": {
               required: true,
               minlength: 3,
-            }
+            },
             "year": {
               required: true,
-            }
+            },
             "month": {
               required: true,
-            }
+            },
             "day": {
               required: true,
-            }
+            },
             "first_name": {
               required: true,
               minlength: 3,
-            }
+            },
             "language_preference": {
               required: true,
             }
@@ -112,34 +102,35 @@ $(function ()
         messages: {
             "email": {
                 required: "You must enter a login name",
-                loginRegex: "Login format not valid"
+                emailRegex: "Login format not valid"
             },
             "password": {
                 required: "Your password a not valid",
-            }
+            },
             "password_confirm": {
               required: "Wrong password",
-            }
+              equalTo : "Enter the correct password"
+            },
             "phone": {
               required: "Required field",
-            }
+            },
             "last_name": {
               required: "Required field",
               minlength: "Enter your full last name",
-            }
+            },
             "year": {
               required: "Required field",
-            }
+            },
             "month": {
               required: "Required field",
-            }
+            },
             "day": {
               required: "Required field",
-            }
+            },
             "first_name": {
               required: "Required field",
               minlength: "Enter your full name",
-            }
+            },
             "language_preference": {
               required: "Required field",
             }
@@ -147,93 +138,85 @@ $(function ()
     });
 });
 
-
-$(".phone").intlTelInput({
-  allowDropdown: true,
-    // options here
+$(function() {
+    $(".phone").intlTelInput({
+      allowDropdown: true,
+        // options here
+    });
 });
 
-
-$(function ()
-{
-
+$(function() {
     $(".form-user-address").validate({
         rules: {
             "email": {
                 required: true,
-                loginRegex: true,
+                emailRegex: true,
             },
             "phone": {
               required: true,
-            }
+            },
             "last_name": {
               required: true,
               minlength: 3,
-            }
+            },
             "first_name": {
               required: true,
               minlength: 3,
-            }
+            },
             "language_preference": {
               required: true,
-            }
+            },
             "street": {
               required: true,
-            }
+            },
             "apt": {
               required: true,
-            }
+            },
             "address": {
               required: true,
-            }
+            },
             "city": {
               required: true,
-            }
+            },
             "zip_code": {
               required: true,
-            }
-            "address_lookup": {
-              required: true,
-            }
+            },
 
         },
         messages: {
             "email": {
                 required: "You must enter a login name",
-                loginRegex: "Login format not valid"
+                emailRegex: "Login format not valid"
             },
             "phone": {
               required: "Required field",
-            }
+            },
             "last_name": {
               required: "Required field",
               minlength: "Enter your full last name",
-            }
+            },
             "first_name": {
               required: "Required field",
               minlength: "Enter your full name",
-            }
+            },
             "language_preference": {
               required: "Required field",
-            }
+            },
             "street": {
               required: "Required field",
-            }
+            },
             "apt": {
               required: "Required field",
-            }
+            },
             "address": {
               required: "Required field",
-            }
+            },
             "city": {
               required: "Required field",
-            }
+            },
             "zip_code": {
               required: "Required field",
-            }
-            "address_lookup": {
-              required: "Required field",
-            }
+            },
         }
     });
 });
